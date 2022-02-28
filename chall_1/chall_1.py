17fc1fa4027d9c3bd15a798100df61fe74dc1dcf
William Mironchuk <wmironchuk@umass.edu>
2022-02-28 13:50:32 -0500

    renamed files to fit with the correct naming conventions of the project

---
+++ chall_1/chall_1.py
@@ -0,0 +1,15 @@
+from typing import Any
+import pwn
+
+pwn.context.clear(arch="amd64")
+
+p = pwn.proccess("/home/cubscout61/Current/CS390R/Projects/project2/chall_1/chall")
+
+
+def solve_process(buffer_len: int = 0, value_set: Any = None,
+                  value_loc: Any = None) -> None:
+    pass
+
+
+def main():
+    pass
